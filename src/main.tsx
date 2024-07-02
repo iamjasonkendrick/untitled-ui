810a086c62ec3b493f76de16569960acc0267bc3
Jason Richard Kendrick <jasonrichardkendrick@gmail.com>
2024-07-02 16:31:36 +0530

    Removed dark theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App.tsx'
-import { ThemeProvider } from './components/theme-provider'
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 root.render(
   <React.StrictMode>
     <Router>
-      <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-        <App />
-      </ThemeProvider>
+      <App />
     </Router>
   </React.StrictMode>,
 )
