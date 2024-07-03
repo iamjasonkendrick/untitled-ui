4ea8f71662bee85d12155abeb2e99bf26e61378b
Jason Richard Kendrick <jasonrichardkendrick@gmail.com>
2024-07-03 10:04:28 +0530

    Modified footer font size

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,32 +9,32 @@ export default function Footer() {
           <h3 className='font-semibold text-lg mb-4'>Product</h3>
           <ul className='space-y-2'>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm '>
                 Overview
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Features
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Solutions <Badge className='bg-green-700'>New</Badge>
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Tutorials
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Pricing
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Releases
               </a>
             </li>
@@ -44,32 +44,32 @@ export default function Footer() {
           <h3 className='font-semibold text-lg mb-4'>Company</h3>
           <ul className='space-y-2'>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 About us
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Careers
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Press
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 News
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Media kit
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Contact
               </a>
             </li>
@@ -79,32 +79,32 @@ export default function Footer() {
           <h3 className='font-semibold text-lg mb-4'>Resources</h3>
           <ul className='space-y-2'>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Blog
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Newsletter
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Events
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Help centre
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Tutorials
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Support
               </a>
             </li>
@@ -114,32 +114,32 @@ export default function Footer() {
           <h3 className='font-semibold text-lg mb-4'>Use cases</h3>
           <ul className='space-y-2'>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Startups
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Enterprise
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Government
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 SaaS centre
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Marketplaces
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Ecommerce
               </a>
             </li>
@@ -149,32 +149,32 @@ export default function Footer() {
           <h3 className='font-semibold text-lg mb-4'>Social</h3>
           <ul className='space-y-2'>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Twitter
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 LinkedIn
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Facebook
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 GitHub
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 AngelList
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Dribbble
               </a>
             </li>
@@ -184,32 +184,32 @@ export default function Footer() {
           <h3 className='font-semibold text-lg mb-4'>Legal</h3>
           <ul className='space-y-2'>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Terms
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Privacy
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Cookies
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Licenses
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Settings
               </a>
             </li>
             <li>
-              <a href='#' className='text-muted-foreground'>
+              <a href='#' className='text-muted-foreground text-sm'>
                 Contact
               </a>
             </li>
@@ -220,7 +220,7 @@ export default function Footer() {
         <div className='flex items-center space-x-2'>
           <LogoWithText />
         </div>
-        <p className='mt-4 text-sm text-muted-foreground md:mt-0'>
+        <p className='mt-4 text-muted-foreground text-sm md:mt-0'>
           &copy; 2077 Untitled UI. All rights reserved.
         </p>
       </div>
--- src/components/trail-section.tsx
+++ src/components/trail-section.tsx
@@ -3,18 +3,20 @@ import { Button } from './ui/button'
 export default function Trail() {
   return (
     <>
-      <div className=' bg-zinc-100 py-12 flex flex-col justify-center items-center'>
-        <h3 className='font-bold text-xl mb-4'>Start your free trial</h3>
-        <p className='font-light mb-8'>
-          Join over 4,000+ startups already growing with Untitled.
-        </p>
-        <div className='flex gap-8'>
-          <Button variant={'outline'} size={'lg'}>
-            Get Started
-          </Button>
-          <Button className='bg-violet-700 hover:bg-violet-900 focus:bg-violet-900'>
-            Get Started
-          </Button>
+      <div className=' bg-zinc-100 py-12 '>
+        <div className='container flex flex-col justify-center items-center'>
+          <h3 className='font-bold text-xl mb-4'>Start your free trial</h3>
+          <p className='font-light mb-8'>
+            Join over 4,000+ startups already growing with Untitled.
+          </p>
+          <div className='flex gap-8'>
+            <Button variant={'outline'} size={'lg'}>
+              Get Started
+            </Button>
+            <Button className='bg-violet-700 hover:bg-violet-900 focus:bg-violet-900'>
+              Get Started
+            </Button>
+          </div>
         </div>
       </div>
     </>
